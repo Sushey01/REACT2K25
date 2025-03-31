f5f42e4f287fc9859c5af14b3d0a175f4dfd5821
Sushey01 <susmagar012@gmail.com>
2025-03-31 22:47:52 +0545

    search-idea

--- sikbabu/src/pages/Home.jsx
+++ sikbabu/src/pages/Home.jsx
@@ -1,9 +1,10 @@
 //Creating a component Home() for entire homepage which will contain many informations!
 // This is a functional component named Home that returns a div with a class name of  "home".
 import MovieCard from "../components/MovieCard"
-
+import {useState} from "react"  //importing useState from react to manage state in functional components
 
 function Home(){
+    const[searchQuery, setSearchQuery] = useState("");
 
 // Variable called movies is created inside the function Home!
     const movies = [
@@ -12,18 +13,27 @@ function Home(){
         {id: 3, title: "Doraemon", release_date:"2003"},
     ];
 
-    const handleSearch =() => {
-
+    const handleSearch =(e) => {
+        e.preventDefault() // Prevents the default form submission behavior
+        // This function is called when the form is submitted. It prevents the default behavior of the form submission and alerts the current value of the searchQuery state variable.
+        alert(searchQuery)
+        setSearchQuery("-------")
     }
     return (
     <div className="home">
 
         <form onSubmit={handleSearch} className="search-form">
-            <input type="text" plcaeholder="Search for movies..." className="search-input"/>
-        </form>
+            <input 
+            type="text" 
+            plcaeholder="Search for movies..." 
+            className="search-input"
+            value={searchQuery} //value of the input field is set to the searchQuery state variable
+            onChange={(e)=> setSearchQuery(e.target.value)}  //onChange event handler updates the searchQuery state variable with the current value of the input field
+            />
+        
 
         <button type="submit" className="search-button">Search</button>
-
+        </form>
 
         {/* grid of the movies */}
         <div className="movies-grid">   
